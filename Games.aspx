4a0ec1df7b5b5b05fb63304068b84f6982c98d26
awilmes <85970174+awilmes@users.noreply.github.com>
2022-05-02 16:19:52 -0500

    Add files via upload

---
+++ Games.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Games.aspx.cs" Inherits="Games" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" data-wf-page="62700d68fc9a4eae0cc03ec9" data-wf-site="62700d68fc9a4e45d3c03ec8">
+<head runat="server">
+    <meta charset="utf-8" />
+    <title>NBA_API</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Webflow" name="generator" />
+    <link href="styles/normalize.css" rel="stylesheet" type="text/css" />
+    <link href="styles/webflow.css" rel="stylesheet" type="text/css" />
+    <link href="styles/nba-api.webflow.css" rel="stylesheet" type="text/css" />
+    <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+    <script type="text/javascript">!function (o, c) { var n = c.documentElement, t = " w-mod-"; n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch") }(window, document);</script>
+    <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <link href="images/webclip.png" rel="apple-touch-icon" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header class="wf-section">
+            <div class="container w-container">
+                <img src="images/nba-logo.png" loading="lazy" width="141" alt="" class="image" />
+                <h1 class="heading">Results from <%# DateTime.Now.AddDays(-1).ToString("D") %></h1>
+            </div>            
+        </header>
+        <div id="main" class="w-container">
+            <asp:Panel ID="pnlNoGames" runat="server" Visible="false">
+                <h2 class="main_heading">No games to display.</h2>
+            </asp:Panel>            
+            <asp:GridView ID="gridGames" runat="server" AutoGenerateColumns="false" OnRowDataBound="OnRowDataBound">
+                <Columns>
+                    <asp:BoundField DataField="date" HeaderText="Date" />
+                    <asp:BoundField DataField="home_team" HeaderText="Home Team" />
+                    <asp:BoundField DataField="home_team_score" HeaderText="Score" />
+                    <asp:BoundField DataField="visitor_team" HeaderText="Visitor Team" />
+                    <asp:BoundField DataField="visitor_team_score" HeaderText="Score" />
+                </Columns>
+            </asp:GridView>
+            <br />
+            <br />
+            <asp:Label ID="lblStatus" runat="server"></asp:Label>
+            <asp:Label ID="lblError" runat="server"></asp:Label>
+        </div>
+        <footer class="section wf-section">
+            <div class="w-container">
+                <div class="text-block">Copyright © 2022 Wilmes Computers LLC. All rights reserved.</div>
+            </div>
+        </footer>
+        <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=62700d68fc9a4e45d3c03ec8" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="js/webflow.js" type="text/javascript"></script>
+        <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+    </form>
+</body>
+</html>
---
+++ Games.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+using NbaApiJson;
+using System.IO;
+
+public partial class Games : System.Web.UI.Page
+{
+    private static HttpClient _client = new HttpClient();
+    private string _json;    
+
+    protected async void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            _json = await GetPrevDayGames();
+            InitializeData();
+            DataBind();            
+        }        
+    }
+
+    protected void InitializeData()
+    {
+        // Creates a list of games based on the JSON returned from the API, and adds the games to a GridView
+        
+        try
+        {
+            List<Datum> games = new List<Datum>();
+
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(_json);
+
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("id");
+            dt1.Columns.Add("date");
+            dt1.Columns.Add("home_team");
+            dt1.Columns.Add("home_team_score");
+            dt1.Columns.Add("visitor_team");
+            dt1.Columns.Add("visitor_team_score");
+
+            for (int i = 0; i < rootObject.Data.Length; i++)
+            {
+                games.Add(rootObject.Data[i]);
+            }
+
+            foreach (Datum game in games)
+            {
+                dt1.Rows.Add(game.Id, game.Date.Date.ToString("D"), game.HomeTeam.Name, game.HomeTeamScore, game.VisitorTeam.Name, game.VisitorTeamScore);
+            }
+
+            gridGames.DataSource = dt1;
+            gridGames.DataBind();
+        }
+        catch (Newtonsoft.Json.JsonReaderException e)
+        {
+            lblError.Text = e.Message;
+        }
+        catch (System.IndexOutOfRangeException e)
+        {
+            pnlNoGames.Visible = true;
+        }
+        catch (Exception err)
+        {
+            lblError.Text = err.Message;
+        }        
+    }
+
+    protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            TableCell homeScoreCell = e.Row.Cells[2];
+            TableCell visitorScoreCell = e.Row.Cells[4];
+
+            int homeScore = int.Parse(homeScoreCell.Text);
+            int visitorScore = int.Parse(visitorScoreCell.Text);
+
+            if (homeScore > visitorScore)
+            {
+                homeScoreCell.BackColor = System.Drawing.ColorTranslator.FromHtml("#82E0AA"); // Sets cell background to green
+                visitorScoreCell.BackColor = System.Drawing.ColorTranslator.FromHtml("#F1948A"); // Sets cell background to red
+            }
+            else if (homeScore < visitorScore)
+            {
+                homeScoreCell.BackColor = System.Drawing.ColorTranslator.FromHtml("#F1948A"); // Sets cell background to red
+                visitorScoreCell.BackColor = System.Drawing.ColorTranslator.FromHtml("#82E0AA"); // Sets cell background to green
+            }
+        }
+    }
+
+    public static string GetPrevDay()
+    {       
+        // Returns a formatted string of the previous day: 'YYYY-MM-DD'
+        
+        string prevDate = DateTime.Now.AddDays(-1).ToShortDateString();
+        string[] splitDate = prevDate.Split('/');
+
+        string outDate = splitDate[2] + "-" + splitDate[0] + "-" + splitDate[1];
+
+        return outDate;
+    }
+
+    public static async Task<string> GetPrevDayGames()
+    {
+        // Gets the JSON of games from the previous day using API
+
+        // Define base URL
+        string prevDay = GetPrevDay();
+        string baseURL = "https://balldontlie.io/api/v1/games?start_date=" + prevDay + "&end_date=" + prevDay;
+        string output;
+
+        try
+        {
+            // Define HttpClient with your first using statement which use a IDisposable.
+            using (_client)
+            {
+                // Initiate the GET request, use the await keyword so it will execute the using statement in order.
+                // The HttpResponseMessage which contains the status code, and data from response.
+                using (HttpResponseMessage res = await _client.GetAsync(baseURL))
+                {
+                    // Get the data from the response and convert it to a C# object.
+                    using (HttpContent content = res.Content)
+                    {
+                        // Assign content to data variable by converting to a string using the await keyword.
+                        var data = await content.ReadAsStringAsync();
+                        // If not null, return log convert the data using newtonsoft JObject Parse class method on the data.
+                        if (data != null)
+                        {
+                            output = data;
+                            return output;
+                        }
+                        else
+                        {
+                            output = "Data was null";
+                            return output;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception err)
+        {
+            return err.Message;
+        }
+    }
+}
