5a46f992152aaf1e9fae7b4a151a3534ba764083
awilmes <85970174+awilmes@users.noreply.github.com>
2022-05-02 16:21:43 -0500

    Update Classes.cs

--- App_Code/Classes.cs
+++ App_Code/Classes.cs
@@ -8,61 +8,9 @@
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
 
-    /// <summary>
-    /// Summary description
-    /// </summary>
-
-    //public class Rootobject
-    //{
-    //    public Datum[] data { get; set; }
-    //    public Meta meta { get; set; }
-    //}
-
-    //public class Meta
-    //{
-    //    public int total_pages { get; set; }
-    //    public int current_page { get; set; }
-    //    public object next_page { get; set; }
-    //    public int per_page { get; set; }
-    //    public int total_count { get; set; }
-    //}
-
-    //public class Datum
-    //{
-    //    public int id { get; set; }
-    //    public DateTime date { get; set; }
-    //    public Home_Team home_team { get; set; }
-    //    public int home_team_score { get; set; }
-    //    public int period { get; set; }
-    //    public bool postseason { get; set; }
-    //    public int season { get; set; }
-    //    public string status { get; set; }
-    //    public string time { get; set; }
-    //    public Visitor_Team visitor_team { get; set; }
-    //    public int visitor_team_score { get; set; }
-    //}
-
-    //public class Home_Team
-    //{
-    //    public int id { get; set; }
-    //    public string abbreviation { get; set; }
-    //    public string city { get; set; }
-    //    public string conference { get; set; }
-    //    public string division { get; set; }
-    //    public string full_name { get; set; }
-    //    public string name { get; set; }
-    //}
-
-    //public class Visitor_Team
-    //{
-    //    public int id { get; set; }
-    //    public string abbreviation { get; set; }
-    //    public string city { get; set; }
-    //    public string conference { get; set; }
-    //    public string division { get; set; }
-    //    public string full_name { get; set; }
-    //    public string name { get; set; }
-    //}
+    <summary>
+        Classes for JSON data
+    </summary>
 
     public partial class RootObject
     {
